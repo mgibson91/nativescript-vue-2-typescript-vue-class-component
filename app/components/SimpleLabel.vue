f42dd6b7f213900254ab9c4c9d140563656c122b
Matt <matthewgibson91@gmail.com>
2018-10-11 01:29:38 +0100

    Adding files

---
+++ app/components/SimpleLabel.vue
@@ -0,0 +1,24 @@
+<template>
+    <Page>
+        <Label class="title" :text="computedText"  col="1"/>
+    </Page>
+</template>
+
+<script lang="ts">
+import Vue from 'nativescript-vue';
+import { Component } from 'vue-property-decorator';
+
+  @Component({})
+  export default class Simple extends Vue{
+    get computedText() {
+      return 'Computed text';
+    }
+  }
+</script>
+
+<style scoped>
+    .title {
+        text-align: left;
+        padding-left: 16;
+    }
+</style>
